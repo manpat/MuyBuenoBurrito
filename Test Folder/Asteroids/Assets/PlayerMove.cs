211718bea61ea896cfdbab0f8717cf09903e1135
manpat <pat@patsown.com>
2014-07-10 11:07:19 +1000

    Changed thing

--- Test Folder/Asteroids/Assets/PlayerMove.cs
+++ Test Folder/Asteroids/Assets/PlayerMove.cs
@@ -2,37 +2,17 @@
 using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
-
 	public float forwardSpeed;
-
-	public float forward;
 	
 	// Use this for initialization
 	void Start () 
 	{
-		forward = Input.GetAxis("Vertical");
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		// Adding velocity in a forward direction
-		if (Input.GetKey(KeyCode.UpArrow))
-		{
-			rigidbody2D.AddForce(transform.up * forward * forwardSpeed);
-		}
-
-
-		// rotating right
-		if (Input.GetKey(KeyCode.RightArrow))
-		{
-			transform.Rotate(0,0,-1.2f);
-		}
-
-		// rotating left
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.Rotate(0,0,1.2f);
-		}
+		rigidbody2D.velocity = transform.up * Input.GetAxis("Vertical") * forwardSpeed;
+		transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * 1.2f);
 	}
 }
