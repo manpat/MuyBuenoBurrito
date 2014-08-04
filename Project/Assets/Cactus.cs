ad1abd20537de2a2c4c7f32a01e7114b66ef8d9f
C-Syratt <c.syratt@aanet.com.au>
2014-08-04 14:50:57 +1000

    Added Cactus Juice
    + Added a Cactus Juice Prefab & Functionality (no drop from cactus)

--- Project/Assets/Cactus.cs
+++ Project/Assets/Cactus.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Cactus : Enemy {
 	protected override void Start () {
-		base.Start();
-		deathTime = 0f;
-	}
+        base.Start();
+        deathTime = 0f;
+    }
 }
